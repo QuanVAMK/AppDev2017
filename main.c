a391a8ad3ac8d763e24c9f500506c661840a3be0
QuanVAMK <nguyenquan261098@gmail.com>
2017-04-23 15:02:54 +0300

    Update main.c
    add comments

--- main.c
+++ main.c
@@ -1,47 +1,47 @@
-/* This program is just for testing */
-#include "wave.h"	//double quotation marks are used for user defined header.
+#include "wave.h"
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 #include <stdlib.h>
-#include "screen.h"
 #include <signal.h>
-#include <sys/wait.h>
+#include "screen.h"
+#include "comm.h"
 
-//This program will open a wave file, and display WAV header info.
-//This program will create a wave file.
-int main(int argc, char *argv[]) {
+//this program will open a wave file, a display WAV header info
+//this program will also create a wave file
+
+int main(int argc, char *argv[]){
 	FILE *fp;
 	WAVHDR myhdr;
-	short int sa[SAMPLE_RATE];
 	int ret;
-/*	if (argc != 2) {
-		printf("Usage: %s wav_file\n", argv[0]);
+	short int sa[SAMPLE_RATE];
+/*	if (argc!=2) 
+	{
+		printf("Usage: %s wav_file\n",argv[0]);
 		return -1;
 	}
-	fp =fopen(argv[1], "r");	//Try to open the wav file.
-	if (fp == NULL) {
-		printf("cannot find file %s\n", argv[1]);
+	fp = fopen(argv[1],"r"); //try to open wav file
+	if (fp==NULL)
+	{
+		printf("Cannot find file %s\n",argv[1]);
 		return -1;
 	}
 */
-	while(1) {
-	gotoXY(1,1);
-	ret = system("arecord -r16000 -c1 -d1 -f S16_LE data.wav");
-	if (WIFSIGNALED(ret) && WTERMSIG(ret) == SIGINT) break;
-	clearScreen();    //Make a clear screen.
-	fp = fopen("data.wav", "r");
-	fread(&myhdr, sizeof(myhdr), 1, fp); //read the wav header.
-	displayWAVHDR(myhdr);		//display the wav headeinfo.
-	fread(&sa, sizeof(short int), SAMPLE_RATE,fp);
-	dispWAVdata(sa);
-	fclose(fp);
-
+	for (;;){
+		ret = system("arecord -r16000 -c1 -d1 -f S16_LE -q data.wav");
+		clearScreen(); //a fresh screen to display
+		if (WIFSIGNALED(ret) && (WTERMSIG(ret)==SIGINT || WTERMSIG(ret)==SIGQUIT)) break;
+		fp = fopen("data.wav","r");
+		fread(&myhdr,sizeof(myhdr),1,fp); //Read wave header info then display it
+		displayWAVHDR(myhdr);
+		fread(&sa, sizeof(short int),SAMPLE_RATE,fp); //This will display the wave data as bars or numbers.
+		displayWAVdata(sa);
+		fclose(fp);
 	}
 /*	int ans;
-	printf("Do you want to genearate a test tone? (1:yes, 0:no)\n");
-	scanf("%d", &answer);
-	if (answer == 1) testTone(12500, 5);
+	printf("Do you want to generate a test tone? (1:yes, 0: no)");
+	scanf("%d",&ans);
+	testTone(440,3);
 */
 	return 0;
 }
-
